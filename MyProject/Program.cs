fca63eceb142f214aae4fb5106d7f526a02d6b27
Denis727 <piligrim727@mail.ru>
2015-08-21 23:09:37 +0300

    Denis727

---
+++ MyProject/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyProject
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+
+        // Функция вычисления площади прямоугольного треугольника по двум катетам
+        public static double AreaRightTriangle(double cathetusA, double cathetusB)
+        {
+            if (cathetusA <= 0.0 || cathetusB <= 0.0) return 0.0; // треугольник не существует
+            return cathetusA * cathetusB / 2.0;
+        }
+
+        // Функция вычисления площади прямоугольного треугольника по трем сторонам
+        public static double AreaRightTriangle(double a, double b, double c)
+        {
+            if (a <= 0.0 || b <= 0.0 || c <= 0.0 || a + b < c || a + c < b || b + c < a)
+                return 0.0; // треугольник не существует
+            if (a > b)
+            {
+                if (a > c && b * b + c * c == a * a) return b * c / 2.0;
+                else if (a * a + b * b == c * c) return a * b / 2.0;
+                else return 0.0; // треугольник не прямоугольный
+            }
+            else
+            {
+                if (b > c && a * a + c * c == b * b) return a * c / 2.0;
+                else if (a * a + b * b == c * c) return a * b / 2.0;
+                else return 0.0; // треугольник не прямоугольный
+            }
+        }
+
+        // Функция вычисления площади любого треугольника по трем сторонам
+        public static double AreaTriangle(double a, double b, double c)
+        {
+            if (a <= 0 || b <= 0 || c <= 0) return 0.0; // треугольник не существует
+            double p = (a + b + c) / 2.0;
+            double s = p * (p - a) * (p - b) * (p - c);
+            if (s > 0.0) return Math.Sqrt(s);
+            else return 0.0; // треугольник не существует
+        }
+    }
+}
---
+++ MyProjectTests/ProgramTests.cs
@@ -0,0 +1,140 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MyProject;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyProject.Tests
+{
+    [TestClass()]
+    public class ProgramTests
+    {
+        [TestMethod()]
+        public void AreaRightTriangle_NormalTwo()
+        {
+            // Arrange
+            double a = 3.0;
+            double b = 4.0;
+            double expectedArea = 6.0;
+
+            // Act
+            double actualArea = Program.AreaRightTriangle(a, b);
+
+            // Assert
+            Assert.AreEqual(expectedArea, actualArea);
+        }
+
+        [TestMethod()]
+        public void AreaRightTriangle_NegativeTwo()
+        {
+            // Arrange
+            double a = 3.0;
+            double b = -4.0;
+            double expectedArea = 0;
+
+            // Act
+            double actualArea = Program.AreaRightTriangle(a, b);
+
+            // Assert
+            Assert.AreEqual(expectedArea, actualArea);
+        }
+
+        [TestMethod()]
+        public void AreaRightTriangle_NormalThree()
+        {
+            // Arrange
+            double a = 3.0;
+            double b = 4.0;
+            double c = 5.0;
+            double expectedArea = 6.0;
+
+            // Act
+            double actualArea = Program.AreaRightTriangle(a, b, c);
+
+            // Assert
+            Assert.AreEqual(expectedArea, actualArea);
+        }
+
+        [TestMethod()]
+        public void AreaRightTriangle_NotExist()
+        {
+            // Arrange
+            double a = 1.0;
+            double b = 2.0;
+            double c = 5.0;
+            double expectedArea = 0.0;
+
+            // Act
+            double actualArea = Program.AreaRightTriangle(a, b, c);
+
+            // Assert
+            Assert.AreEqual(expectedArea, actualArea);
+        }
+
+        [TestMethod()]
+        public void AreaRightTriangle_NegativeThree()
+        {
+            // Arrange
+            double a = 3.0;
+            double b = 4.0;
+            double c = -5.0;
+            double expectedArea = 0.0;
+
+            // Act
+            double actualArea = Program.AreaRightTriangle(a, b, c);
+
+            // Assert
+            Assert.AreEqual(expectedArea, actualArea);
+        }
+
+        [TestMethod()]
+        public void AreaTriangle_Normal()
+        {
+            // Arrange
+            double a = 3.0;
+            double b = 4.0;
+            double c = 5.0;
+            double expectedArea = 6.0;
+
+            // Act
+            double actualArea = Program.AreaTriangle(a, b, c);
+
+            // Assert
+            Assert.AreEqual(expectedArea, actualArea);
+        }
+
+        [TestMethod()]
+        public void AreaTriangle_NotExist()
+        {
+            // Arrange
+            double a = 2.0;
+            double b = 1.0;
+            double c = 5.0;
+            double expectedArea = 0.0;
+
+            // Act
+            double actualArea = Program.AreaTriangle(a, b, c);
+
+            // Assert
+            Assert.AreEqual(expectedArea, actualArea);
+        }
+
+        [TestMethod()]
+        public void AreaTriangle_Negative()
+        {
+            // Arrange
+            double a = 2.0;
+            double b = 1.0;
+            double c = -5.0;
+            double expectedArea = 0.0;
+
+            // Act
+            double actualArea = Program.AreaTriangle(a, b, c);
+
+            // Assert
+            Assert.AreEqual(expectedArea, actualArea);
+        }
+    }
+}
